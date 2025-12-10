f46e4dedcc87c22ca5453e3487c92a227cce128b
Norbert250 <norbertndayisenga@gmail.com>
2025-12-10 14:48:33 +0200

    Fix asset analysis API URL to use HTTPS for Vercel deployment

--- src/components/apply/StepThree.tsx
+++ src/components/apply/StepThree.tsx
@@ -95,9 +95,9 @@ export const StepThree = ({ formData, updateFormData, nextStep, prevStep, trackF
       formData.append('files', file);
     });
     
-    console.log('Sending to Assets API:', 'http://157.245.20.199:8000/api/v1/process-images');
+    console.log('Sending to Assets API:', 'https://157.245.20.199:8000/api/v1/process-images');
     
-    const response = await fetch('http://157.245.20.199:8000/api/v1/process-images', {
+    const response = await fetch('https://157.245.20.199:8000/api/v1/process-images', {
       method: 'POST',
       body: formData,
     });
